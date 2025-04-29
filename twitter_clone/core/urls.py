3e10c3619b59a50935973e8220838f1dfbfc0e3e
mitch104 <mitch_10_4@hotmail.com>
2025-04-29 20:15:15 +0100

    Use class based views

--- twitter_clone/core/urls.py
+++ twitter_clone/core/urls.py
@@ -3,15 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('explore/', views.explore, name='explore'),
-    path('users/', views.users_list, name='users_list'),
-    path('tweet/<int:tweet_id>/', views.tweet_detail, name='tweet_detail'),
-    path('tweet/new/', views.new_tweet, name='new_tweet'),
-    path('tweet/<int:tweet_id>/like/', views.like_tweet, name='like_tweet'),
-    path('tweet/<int:tweet_id>/retweet/', views.retweet, name='retweet'),
-    path('profile/<str:username>/', views.profile, name='profile'),
-    path('profile/<str:username>/follow/', views.follow_user, name='follow_user'),
-    path('edit_profile/', views.edit_profile, name='edit_profile'),
-    path('register/', views.register, name='register'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('explore/', views.ExploreView.as_view(), name='explore'),
+    path('users/', views.UsersListView.as_view(), name='users_list'),
+    path('tweet/<int:pk>/', views.TweetDetailView.as_view(), name='tweet_detail'),
+    path('tweet/new/', views.NewTweetView.as_view(), name='new_tweet'),
+    path('tweet/<int:tweet_id>/like/', views.LikeTweetView.as_view(), name='like_tweet'),
+    path('tweet/<int:tweet_id>/retweet/', views.RetweetView.as_view(), name='retweet'),
+    path('profile/<str:username>/', views.ProfileView.as_view(), name='profile'),
+    path('profile/<str:username>/follow/', views.FollowUserView.as_view(), name='follow_user'),
+    path('edit_profile/', views.EditProfileView.as_view(), name='edit_profile'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ] 
--- twitter_clone/core/views.py
+++ twitter_clone/core/views.py
@@ -1,194 +1,216 @@
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    FormView,
+    ListView,
+    UpdateView,
+    View,
+)
 
 from .forms import ProfileUpdateForm, TweetForm, UserRegisterForm, UserUpdateForm
-from .models import Follow, Like, Profile, Tweet
+from .models import Follow, Like, Tweet
 
 
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}! You can now log in')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'core/register.html', {'form': form})
+class RegisterView(CreateView):
+    form_class = UserRegisterForm
+    template_name = 'core/register.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, f'Account created for {form.cleaned_data.get("username")}! You can now log in')
+        return response
 
 
-@login_required
-def home(request):
-    following = Follow.objects.filter(follower=request.user).values_list('following', flat=True)
-    tweets = Tweet.objects.filter(Q(user__in=following) | Q(user=request.user)).order_by('-created_at')
-    
-    # Get tweets liked by the user
-    liked_tweets = []
-    if request.user.is_authenticated:
-        liked_tweets = Like.objects.filter(user=request.user).values_list('tweet_id', flat=True)
-    
-    if request.method == 'POST':
-        form = TweetForm(request.POST, request.FILES)
-        if form.is_valid():
-            tweet = form.save(commit=False)
-            tweet.user = request.user
-            tweet.save()
-            messages.success(request, 'Your tweet has been posted!')
-            return redirect('home')
-    else:
-        form = TweetForm()
+class HomeView(LoginRequiredMixin, FormView):
+    template_name = 'core/home.html'
+    form_class = TweetForm
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        following = Follow.objects.filter(follower=self.request.user).values_list('following', flat=True)
+        context['tweets'] = Tweet.objects.filter(
+            Q(user__in=following) | Q(user=self.request.user)
+        ).order_by('-created_at')
+        context['liked_tweets'] = Like.objects.filter(
+            user=self.request.user
+        ).values_list('tweet_id', flat=True)
+        return context
+
+    def form_valid(self, form):
+        tweet = form.save(commit=False)
+        tweet.user = self.request.user
+        tweet.save()
+        messages.success(self.request, 'Your tweet has been posted!')
+        return super().form_valid(form)
+
+
+class ExploreView(ListView):
+    template_name = 'core/explore.html'
+    context_object_name = 'tweets'
+    model = Tweet
+    ordering = ['-created_at']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context['liked_tweets'] = Like.objects.filter(
+                user=self.request.user
+            ).values_list('tweet_id', flat=True)
+        return context
+
+
+class UsersListView(ListView):
+    template_name = 'core/users_list.html'
+    context_object_name = 'users'
+    model = User
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.is_authenticated:
+            queryset = queryset.exclude(id=self.request.user.id)
         
-    context = {
-        'tweets': tweets,
-        'form': form,
-        'liked_tweets': liked_tweets
-    }
-    return render(request, 'core/home.html', context)
+        search_query = self.request.GET.get('search', '')
+        if search_query:
+            queryset = queryset.filter(
+                Q(username__icontains=search_query) | 
+                Q(email__icontains=search_query)
+            )
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context['following'] = Follow.objects.filter(
+                follower=self.request.user
+            ).values_list('following_id', flat=True)
+        context['search_query'] = self.request.GET.get('search', '')
+        return context
 
 
-def explore(request):
-    tweets = Tweet.objects.all().order_by('-created_at')
-    
-    # Get tweets liked by the user
-    liked_tweets = []
-    if request.user.is_authenticated:
-        liked_tweets = Like.objects.filter(user=request.user).values_list('tweet_id', flat=True)
-    
-    return render(request, 'core/explore.html', {'tweets': tweets, 'liked_tweets': liked_tweets})
+class TweetDetailView(DetailView):
+    template_name = 'core/tweet_detail.html'
+    model = Tweet
+    context_object_name = 'tweet'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context['liked'] = Like.objects.filter(
+                user=self.request.user,
+                tweet=self.object
+            ).exists()
+        return context
 
 
-def users_list(request):
-    # Get all users except the current user
-    users = User.objects.exclude(id=request.user.id) if request.user.is_authenticated else User.objects.all()
-    
-    # Get the users followed by the current user
-    following = []
-    if request.user.is_authenticated:
-        following = Follow.objects.filter(follower=request.user).values_list('following_id', flat=True)
-    
-    # Filter users by search query if provided
-    search_query = request.GET.get('search', '')
-    if search_query:
-        users = users.filter(
-            Q(username__icontains=search_query) | 
-            Q(email__icontains=search_query)
-        )
-    
-    context = {
-        'users': users,
-        'following': following,
-        'search_query': search_query
-    }
-    return render(request, 'core/users_list.html', context)
+class NewTweetView(LoginRequiredMixin, CreateView):
+    template_name = 'core/new_tweet.html'
+    form_class = TweetForm
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, 'Your tweet has been posted!')
+        return super().form_valid(form)
 
 
-def tweet_detail(request, tweet_id):
-    tweet = get_object_or_404(Tweet, id=tweet_id)
-    
-    # Check if the user liked this tweet
-    liked = False
-    if request.user.is_authenticated:
-        liked = Like.objects.filter(user=request.user, tweet=tweet).exists()
-    
-    return render(request, 'core/tweet_detail.html', {'tweet': tweet, 'liked': liked})
-
-
-@login_required
-def new_tweet(request):
-    if request.method == 'POST':
-        form = TweetForm(request.POST, request.FILES)
-        if form.is_valid():
-            tweet = form.save(commit=False)
-            tweet.user = request.user
-            tweet.save()
-            messages.success(request, 'Your tweet has been posted!')
-            return redirect('home')
-    else:
-        form = TweetForm()
-    return render(request, 'core/new_tweet.html', {'form': form})
-
-
-@login_required
-def like_tweet(request, tweet_id):
-    tweet = get_object_or_404(Tweet, id=tweet_id)
-    like, created = Like.objects.get_or_create(user=request.user, tweet=tweet)
-    
-    if not created:
-        like.delete()
-        liked = False
-    else:
-        liked = True
+class LikeTweetView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        tweet = get_object_or_404(Tweet, id=kwargs['tweet_id'])
+        like, created = Like.objects.get_or_create(user=request.user, tweet=tweet)
         
-    # Get updated liked tweets list for the template
-    liked_tweets = Like.objects.filter(user=request.user).values_list('tweet_id', flat=True)
-    
-    return render(request, 'core/tweet_card.html', {
-        'tweet': tweet,
-        'liked_tweets': liked_tweets,
-        'retweeted_tweets': []  # Add this if you want to track retweets too
-    })
-
-
-@login_required
-def retweet(request, tweet_id):
-    original_tweet = get_object_or_404(Tweet, id=tweet_id)
-    
-    # Check if user already retweeted this tweet
-    existing_retweet = Tweet.objects.filter(parent=original_tweet, user=request.user).first()
-    
-    if existing_retweet:
-        existing_retweet.delete()
-        retweeted = False
-    else:
-        retweet = Tweet(user=request.user, content=original_tweet.content, parent=original_tweet)
-        if original_tweet.image:
-            retweet.image = original_tweet.image
-        retweet.save()
-        retweeted = True
-    
-    # Get updated retweeted tweets list for the template
-    retweeted_tweets = Tweet.objects.filter(
-        parent=original_tweet,
-        user=request.user
-    ).values_list('parent_id', flat=True)
-    
-    return render(request, 'core/tweet_card.html', {
-        'tweet': original_tweet,
-        'liked_tweets': Like.objects.filter(user=request.user).values_list('tweet_id', flat=True),
-        'retweeted_tweets': retweeted_tweets
-    })
-
-
-def profile(request, username):
-    user = get_object_or_404(User, username=username)
-    tweets = Tweet.objects.filter(user=user).order_by('-created_at')
-    
-    # Get tweets liked by the user
-    liked_tweets = []
-    if request.user.is_authenticated:
+        if not created:
+            like.delete()
+        
         liked_tweets = Like.objects.filter(user=request.user).values_list('tweet_id', flat=True)
-    
-    is_following = False
-    if request.user.is_authenticated:
-        is_following = Follow.objects.filter(follower=request.user, following=user).exists()
-    
-    context = {
-        'profile_user': user,
-        'tweets': tweets,
-        'is_following': is_following,
-        'liked_tweets': liked_tweets
-    }
-    return render(request, 'core/profile.html', context)
+        
+        return render(request, 'core/tweet_card.html', {
+            'tweet': tweet,
+            'liked_tweets': liked_tweets,
+            'retweeted_tweets': []
+        })
 
 
-@login_required
-def edit_profile(request):
-    if request.method == 'POST':
+class RetweetView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        original_tweet = get_object_or_404(Tweet, id=kwargs['tweet_id'])
+        existing_retweet = Tweet.objects.filter(
+            parent=original_tweet,
+            user=request.user
+        ).first()
+        
+        if existing_retweet:
+            existing_retweet.delete()
+        else:
+            retweet = Tweet(
+                user=request.user,
+                content=original_tweet.content,
+                parent=original_tweet
+            )
+            if original_tweet.image:
+                retweet.image = original_tweet.image
+            retweet.save()
+        
+        retweeted_tweets = Tweet.objects.filter(
+            parent=original_tweet,
+            user=request.user
+        ).values_list('parent_id', flat=True)
+        
+        return render(request, 'core/tweet_card.html', {
+            'tweet': original_tweet,
+            'liked_tweets': Like.objects.filter(user=request.user).values_list('tweet_id', flat=True),
+            'retweeted_tweets': retweeted_tweets
+        })
+
+
+class ProfileView(DetailView):
+    template_name = 'core/profile.html'
+    model = User
+    context_object_name = 'profile_user'
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tweets'] = Tweet.objects.filter(
+            user=self.object
+        ).order_by('-created_at')
+        
+        if self.request.user.is_authenticated:
+            context['liked_tweets'] = Like.objects.filter(
+                user=self.request.user
+            ).values_list('tweet_id', flat=True)
+            context['is_following'] = Follow.objects.filter(
+                follower=self.request.user,
+                following=self.object
+            ).exists()
+        
+        return context
+
+
+class EditProfileView(LoginRequiredMixin, UpdateView):
+    template_name = 'core/edit_profile.html'
+    success_url = reverse_lazy('profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.method == 'GET':
+            context['u_form'] = UserUpdateForm(instance=self.request.user)
+            context['p_form'] = ProfileUpdateForm(instance=self.request.user.profile)
+        return context
+
+    def post(self, request, *args, **kwargs):
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
         
@@ -197,44 +219,39 @@ def edit_profile(request):
             p_form.save()
             messages.success(request, 'Your profile has been updated!')
             return redirect('profile', username=request.user.username)
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-    
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
-    return render(request, 'core/edit_profile.html', context)
-
-
-@login_required
-def follow_user(request, username):
-    user_to_follow = get_object_or_404(User, username=username)
-    
-    if user_to_follow == request.user:
-        messages.warning(request, "You cannot follow yourself")
-        return redirect('profile', username=username)
-    
-    follow, created = Follow.objects.get_or_create(follower=request.user, following=user_to_follow)
-    
-    if not created:
-        follow.delete()
-        is_following = False
-        messages.success(request, f'You unfollowed {username}')
-    else:
-        is_following = True
-        messages.success(request, f'You are now following {username}')
-    
-    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-        return JsonResponse({
-            'is_following': is_following,
-            'followers_count': user_to_follow.profile.get_followers_count()
-        })
-    
-    # Redirect back to the page that initiated the follow action
-    referer = request.META.get('HTTP_REFERER')
-    if referer and 'users' in referer:
-        return redirect('users_list')
         
-    return redirect('profile', username=username) 
+        return self.render_to_response(self.get_context_data(u_form=u_form, p_form=p_form))
+
+
+class FollowUserView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        user_to_follow = get_object_or_404(User, username=kwargs['username'])
+        
+        if user_to_follow == request.user:
+            messages.warning(request, "You cannot follow yourself")
+            return redirect('profile', username=kwargs['username'])
+        
+        follow, created = Follow.objects.get_or_create(
+            follower=request.user,
+            following=user_to_follow
+        )
+        
+        if not created:
+            follow.delete()
+            is_following = False
+            messages.success(request, f'You unfollowed {kwargs["username"]}')
+        else:
+            is_following = True
+            messages.success(request, f'You are now following {kwargs["username"]}')
+        
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            return JsonResponse({
+                'is_following': is_following,
+                'followers_count': user_to_follow.profile.get_followers_count()
+            })
+        
+        referer = request.META.get('HTTP_REFERER')
+        if referer and 'users' in referer:
+            return redirect('users_list')
+            
+        return redirect('profile', username=kwargs['username']) 
