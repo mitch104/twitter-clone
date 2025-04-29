4de08c58693dc13758cd759acbd8961f68d6a97c
mitch104 <mitch_10_4@hotmail.com>
2025-04-29 22:27:57 +0100

    Fix docker setup and remove footer

--- twitter_clone/twitter_clone/urls.py
+++ twitter_clone/twitter_clone/urls.py
@@ -2,12 +2,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from django.urls.resolvers import URLPattern, URLResolver
 
 urlpatterns: list[URLPattern | URLResolver] = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG:
