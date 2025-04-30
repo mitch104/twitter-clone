54172caf04aeea5e058342f77de04ba748a15956
mitch104 <mitch_10_4@hotmail.com>
2025-04-30 19:45:01 +0100

    Implement infinite scrolling

--- twitter_clone/core/views.py
+++ twitter_clone/core/views.py
@@ -44,15 +44,26 @@ class HomeView(LoginRequiredMixin, TweetContextMixin, ListView):
     template_name = "core/home.html"
     model = Tweet
     context_object_name = "tweets"
-    paginate_by = 10
+    paginate_by = 4
 
     def get_queryset(self) -> QuerySet[Tweet]:
         following = Follow.objects.filter(follower=self.request.user).values_list("following", flat=True)
         return Tweet.objects.filter(Q(user__in=following) | Q(user=self.request.user)).order_by("-created_at")
 
+    def get_template_names(self) -> list[str]:
+        if self.request.htmx:
+            return ["core/timeline.html"]
+        return [self.template_name]
+
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        # Just to make infinite scrolling show spinner to make it more obvious
+        if self.request.htmx:
+            import time
+
+            time.sleep(1)
         context: dict[str, Any] = super().get_context_data(**kwargs)
-        context["form"] = TweetForm()
+        if not self.request.htmx:
+            context["form"] = TweetForm()
         return context
 
 
