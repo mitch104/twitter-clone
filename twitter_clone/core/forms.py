94c4b040ab4b47b526c17dcefff4385b98bb6898
mitch104 <mitch_10_4@hotmail.com>
2025-04-30 19:02:01 +0100

    Reduce complexity of UserRegisterForm

--- twitter_clone/core/forms.py
+++ twitter_clone/core/forms.py
@@ -14,14 +14,6 @@ class UserRegisterForm(UserCreationForm):
         model = CustomUser
         fields = ["username", "email", "bio", "password1", "password2"]
 
-    def clean(self) -> dict[str, Any]:
-        cleaned_data: dict[str, Any] = super().clean()
-        password1 = cleaned_data.get("password1")
-        password2 = cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return cleaned_data
-
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
